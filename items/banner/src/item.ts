1dd2531a6acc28c111d425330ecd7a35735e3247
espoal <alberto.websci@gmail.com>
2022-03-04 17:54:48 +0100

    chore: debugging

--- items/banner/src/item.ts
+++ items/banner/src/item.ts
@@ -2,38 +2,6 @@ import { getParcel } from '@decentraland/ParcelIdentity'
 
 const fireEvent = (event: any, msg: any) => log('event!')
 
-export const registerDclEvents = async () => {
-
-    let enterTime = 0.0
-    let connectionTime = 0.0
-
-    const parcel = await getParcel()
-    const { cid: parcelId } = parcel
-    const baseParcel = parcel.land.sceneJsonData.scene.base
-
-    onEnterSceneObservable.add(async (player) => {
-        enterTime = Date.now()
-        log('enter!!!')
-        fireEvent('enterLand',{parcelId, baseParcel})
-    })
-
-    onLeaveSceneObservable.add(player => {
-        const duration = Math.floor(Date.now() - enterTime)
-        fireEvent('visitDuration', {parcelId, baseParcel, duration})
-    })
-
-    onPlayerConnectedObservable.add(player => {
-        connectionTime = Date.now()
-        fireEvent('connectLand',{parcelId, baseParcel})
-    })
-
-    onPlayerDisconnectedObservable.add(player => {
-        const duration = Math.floor(Date.now() - connectionTime)
-        fireEvent('connectionDuration', {parcelId, baseParcel, duration})
-    })
-
-    return {parcelId, baseParcel}
-}
 
 export default class AnalyticBanner {
     init() {}
@@ -44,9 +12,9 @@ export default class AnalyticBanner {
 
 
         // const itemId = baseParcel.replace('-','m')
-        const itemId = 5858
+        const itemId = 'sciArt'
         // TODO: compute customerId
-        const customerId = itemId
+        const customerId = 'hTMR5yjBRdct61jGs7CzEPAFm3l2'
         const url = `https://europe-west3-backend-339310.cloudfunctions.net/serve/${customerId}-${itemId}.jpg`
 
         let QRPlane = new Entity()
